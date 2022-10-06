c176b00e3a60cac7b5ec08428fbfb48060e302d5
Joseph <jsltran@gmail.com>
2022-10-05 21:52:31 -0700

    commit tree edda7f3062ecc373ff6d017e91267dfc5cbcd955

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
+# class Person
+#   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+#   def initialize(...all that stuff)
+#     @all that stuff = all that stuff
+#   end
+# end
+
+# refactored
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      #create getter/setter by calling the attr_accessor method instead of writing all the args and assigning them
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end 
+  end 
 end
+
+# :name, :birthday, :hair_color, :eye_color, :height,
+# :weight, :handed, :complexion, :t_shirt_size,
+# :wrist_size, :glove_size, :pant_length, :pant_width
